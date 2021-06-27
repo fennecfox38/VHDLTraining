9305bfc1e3149970b0f0a95923708517c68e3c53
fennecfox38 <fennecfox38.@gmail.com>
2021-06-27 16:56:37 +0900

    3-to-8 Decoder (Behavioral)

---
+++ 3-to-8 Decoder (Behavioral)/design.vhd
@@ -0,0 +1,22 @@
+--https://www.edaplayground.com/x/Gfkw
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity Decoder_3_8 is
+    port (
+        Input  : in std_logic_vector(2 downto 0);
+        Output : out std_logic_vector(7 downto 0));
+end Decoder_3_8;
+
+architecture Behavior of Decoder_3_8 is
+begin
+    with Input select
+        Output <= "00000001" when "000",
+        "00000010" when "001",
+        "00000100" when "010",
+        "00001000" when "011",
+        "00010000" when "100",
+        "00100000" when "101",
+        "01000000" when "110",
+        "10000000" when others;
+end Behavior;
---
+++ 3-to-8 Decoder (Behavioral)/testbench.vhd
@@ -0,0 +1,35 @@
+--https://www.edaplayground.com/x/Gfkw
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity TestBench is
+end TestBench;
+
+architecture TestBench of TestBench is
+    component Decoder_3_8 is
+        port (
+            Input  : in std_logic_vector(2 downto 0);
+            Output : out std_logic_vector(7 downto 0));
+    end component;
+
+    signal Input  : std_logic_vector(2 downto 0) := "000";
+    signal Output : std_logic_vector(7 downto 0);
+
+begin
+    dut : Decoder_3_8 port map(Input => Input, Output => Output);
+
+    process begin
+        wait for 10 ns;
+        Input(0) <= not Input(0);
+    end process;
+
+    process begin
+        wait for 20 ns;
+        Input(1) <= not Input(1);
+    end process;
+
+    process begin
+        wait for 40 ns;
+        Input(2) <= not Input(2);
+    end process;
+end TestBench;
